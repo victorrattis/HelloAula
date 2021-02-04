50b1f0fbca9ed7096e5f961736d05267590e999a
android@-build <android@build.com>
2021-02-04 20:07:50 -0300

    Adicionar get system property

--- src/com/study/platform/helloaula/MainActivity.java
+++ src/com/study/platform/helloaula/MainActivity.java
@@ -5,6 +5,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.widget.TextView;
+import android.os.SystemProperties;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -13,10 +17,19 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-	TelephonyManager telephonyManager = new TelephonyManager(this);
-	String result = telephonyManager.getImei(0);
+	//TelephonyManager telephonyManager = new TelephonyManager(this);
+	// String result = telephonyManager.getImei(0);
 
-	((TextView) findViewById(R.id.text)).setText(result);
+	((Button) findViewById(R.id.btn_get_property)).setOnClickListener(new View.OnClickListener() {
+             public void onClick(View v) {
+             	getSystemProperty();
+	     }
+         });
+    }
 
+    private void getSystemProperty() {
+	String name = ((EditText) findViewById(R.id.edit_property_name)).getText().toString();
+    	String result = SystemProperties.get(name);
+    	((TextView) findViewById(R.id.text)).setText(result);
     }
 }
